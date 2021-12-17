d06f77c5374d237c0189ec553f056ffc35eb6f5d
KevinKaras <kevinkaras2@gmail.com>
2021-12-17 12:12:00 -0500

    added commit for graacks raspberry pi project

--- script2.js
+++ script2.js
@@ -18,7 +18,7 @@ sensor.getAll(function (err, tempObj) {
     console.log("This will be an object with all the temperatures graack", tempObj);
 });
 
-sensor.get('28-3c01f0968d31', function (err, temp) {
+sensor.get('28-3c01f0968d31' , function (err, temp) {
     console.log(temp);
     document.getElementById("temperature").innerText = "Temp: " + temp
 });
